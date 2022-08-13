83f7cd8dd4dbae8749d267eba8c46911de14c8e0
Josh Muleshi <jmuleshi2@gmail.com>
2022-08-13 23:20:57 +0200

    add home viewmodel

--- app/src/main/java/cd/wayupdev/repstudiant/ui/home/business/HomeViewModel.kt
+++ app/src/main/java/cd/wayupdev/repstudiant/ui/home/business/HomeViewModel.kt
@@ -1,4 +1,24 @@
 package cd.wayupdev.repstudiant.ui.home.business
 
-class HomeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import cd.wayupdev.repstudiant.data.entity.Etudiant
+import cd.wayupdev.repstudiant.data.repository.EtudiantRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val etudiantRepository : EtudiantRepository
+) : ViewModel() {
+    val findAll = etudiantRepository.findAll().asLiveData()
+
+    fun findByid(etudiantID : Int): LiveData<Etudiant>{
+        return etudiantRepository.findByid(etudiantID).asLiveData()
+    }
+
+    fun searchDatabase(searchQuery: String): LiveData<List<Etudiant>>{
+        return etudiantRepository.searchDatabase(searchQuery).asLiveData()
+    }
 }
