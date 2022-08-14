e346956f8e7006d84db02cc897f0f08fe1033672
Josh Muleshi <jmuleshi2@gmail.com>
2022-08-14 21:52:02 +0200

    added an intent to navigate from home to addEtudiant activity

--- app/src/main/java/cd/wayupdev/repstudiant/ui/home/HomeActivity.kt
+++ app/src/main/java/cd/wayupdev/repstudiant/ui/home/HomeActivity.kt
@@ -1,12 +1,21 @@
 package cd.wayupdev.repstudiant.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cd.wayupdev.repstudiant.R
+import cd.wayupdev.repstudiant.ui.addEtudiant.AddEtudaintActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this@HomeActivity, AddEtudaintActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
