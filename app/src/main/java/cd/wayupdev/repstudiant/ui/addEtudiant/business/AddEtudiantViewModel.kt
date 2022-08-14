56d2da1cc55ddd702c1742496572dfbe7f6b1df3
Josh Muleshi <jmuleshi2@gmail.com>
2022-08-14 20:13:39 +0200

    business logic done

--- app/src/main/java/cd/wayupdev/repstudiant/ui/addEtudiant/business/AddEtudiantViewModel.kt
+++ app/src/main/java/cd/wayupdev/repstudiant/ui/addEtudiant/business/AddEtudiantViewModel.kt
@@ -1,12 +1,21 @@
 package cd.wayupdev.repstudiant.ui.addEtudiant.business
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cd.wayupdev.repstudiant.data.entity.Etudiant
 import cd.wayupdev.repstudiant.data.repository.EtudiantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AddEtudiantViewModel @Inject constructor(
     private val etudiantRepository: EtudiantRepository
-){
-
+) : ViewModel() {
+    fun insertData(etudiant: Etudiant){
+        viewModelScope.launch(Dispatchers.IO){
+            etudiantRepository.insertData(etudiant)
+        }
+    }
 }
